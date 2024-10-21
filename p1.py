d67064ce1c73c74653809fa58f38d8e275d8533c
Rushil <2023ucp1834@mnit.ac.in>
2024-10-21 14:52:01 +0530

    Adding files

---
+++ p1.py
@@ -0,0 +1,11 @@
+print("Hello World")
+a1=int(input("A1:"))
+b1=int(input("B1:"))
+c1=int(input("C1:"))
+a2=int(input("A2:"))
+b2=int(input("B2:"))
+c3=int(input("C2:"))
+
+dot_product= a1*a2+ b1*b2 +c1*c2
+print("Dot product is ", dot_product )\
+print("Hiii")
